dcb0de1184234f3bedf81498b2b8cb1898efb568
jjunchen <jchen0318@gmail.com>
2017-10-17 17:12:18 -0700

    complete bare min requirement part

--- src/dancer.js
+++ src/dancer.js
@@ -12,13 +12,17 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
 
   this.step();  
-
   // // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // // this one sets the position to some random default point within the body
   this.setPosition(top, left);
-  this.$node.on('mouseover', function(event) {
-    console.log(event);
-  });
+  this.$node.on('mouseover', (function(event) {
+    this.$node.css({'height': 40, 'width': 40});
+  }).bind(this));
+  this.$node.on('mouseout', (function() {
+    this.$node.css({'height': 20, 'width': 20});
+  }).bind(this));
+
+  this.moveCloser();
   // return dancer;
 };
 
@@ -50,6 +54,50 @@ makeDancer.prototype.lineUp = function (top, left, counter) {
     };
   }
   this.$node.css(styleSettings);
+};
+
+makeDancer.prototype.moveCloser = function () {
+  var distances = [];
+  var stars = [];
+  for (let i = 0; i < window.dancers.length; i++) {
+    //dancer could be a star
+    if (window.dancers[i].constructor === makeExpandDancer && window.dancers[i] !== this) {
+      var location = window.dancers[i].$node.position();
+      var x = location.left - this.left;
+      var y = location.top - this.top;
+      var dis = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+      if (dis < 100) {
+        stars.push(window.dancers[i]);
+        distances.push(dis);
+      }
+    }
+  } 
+  if (distances.length > 0) {
+    var index = distances.indexOf(Math.min.apply(null, distances));
+    var closestDancer = stars[index];
+    var closestDancerPos = closestDancer.$node.position();
+    var move;
+    if (this.left - closestDancerPos.left > 0) {
+      move = setInterval((function() {
+        this.left -= 20;
+        this.top += 20;
+        this.$node.css({'left': this.left, 'top': this.top});
+      }).bind(this), 500);
+      var stop = setTimeout(function() {
+        clearInterval(move);
+      }, 500);
+    } else {
+      move = setInterval((function() {
+        this.left += 20;
+        this.top -= 20;
+        this.$node.css({'left': this.left, 'top': this.top});
+      }).bind(this), 500);
+      var stop = setTimeout(function() {
+        clearInterval(move);
+      }, 500);
+    }
+  }
+  setTimeout(this.moveCloser.bind(this), 500);
 };
 
 
--- src/expandDancer.js
+++ src/expandDancer.js
@@ -1,11 +1,13 @@
 var makeExpandDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
+  this.move;
   // var blinkyDancer = new makeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   this.$node.prepend('<img id="star" src="src/Gold_Star.svg" />');
-  this.$node.css({'border': 'none', 'height': '5%', 'width': '5%'});
+  // this.$node.css({'border': 'none', 'height': '5%', 'width': '5%'});
+  this.$node.css({'border': 'none', 'height': 20, 'width': 20});
 };
 
 makeExpandDancer.prototype = Object.create(makeDancer.prototype);
@@ -19,7 +21,8 @@ makeExpandDancer.prototype.step = function() {
   var self = this;
   // call the old version of step at the beginning of any call to this new version of step
   // makeDancer.prototype.step.call(this);
-  setInterval(function() {
+  
+  this.move = setInterval(function() {
     left += 20;
     top += 20;
     if (left > width) {
@@ -29,15 +32,25 @@ makeExpandDancer.prototype.step = function() {
       top = 0;
     }
     self.$node.css({'left': left, 'top': top});
-  }, 50);
-  // var top = this.top - 10;
-  // var self = this;
-  // toggle() is a jQuery method to show/hide the <span> tag.
-  // See http://api.jquery.com/category/effects/ for this and
-  // other effects you can use on a jQuery-wrapped html tag.
-  // var randomSize = JSON.stringify(Math.floor(Math.random() * 50));
-  // setInterval(function() {
-  //   self.$node.css({'top': top, 'left': left});
-  // }, 10);
-  // this.$node.css({'left': left});
+  }, 100);
+
+  // var stop = setTimeout(function() {
+  // return function() {
+  //   clearInterval(move);
+  // };
+  // if (this.isMoving) {
+  //   clearInterval(move);
+  // }
+  
 };
+
+makeExpandDancer.prototype.lineUp = function(top, left, counter) {
+  if (counter % 2 === 0) {
+    this.step();
+  } else {
+    var stop = clearInterval(this.move);
+  }
+  makeDancer.prototype.lineUp.call(this, top, left, counter);
+};
+
+makeExpandDancer.prototype.moveCloser = function() {};
